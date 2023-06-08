7c5d5a10c048e5df528c02afc71f2ecda19f70cc
kirato <capacity1412@gmail.com>
2023-06-08 23:54:55 +0700

    done for work

--- main.py
+++ main.py
@@ -6,22 +6,36 @@ def clear_console():
 
 def main(arg):
     result = [random.randint(30, 40), random.randint(30, 40)]
-    result.append(arg - sum(result))
-    
+    max_val = min(arg - sum(result), 20)
+
+    if max_val >= 12:
+        result.append(random.randint(12, max_val))
+    else:
+        result.append(12)
+
     while not (12 <= result[2] <= 20):
         result = [random.randint(30, 40), random.randint(30, 40)]
-        result.append(arg - sum(result))
-    
+        max_val = min(arg - sum(result), 20)
+
+        if max_val >= 12:
+            result.append(random.randint(12, max_val))
+        else:
+            result.append(12)
+
     print(result)
 
 clear_console()
 
 while True:
-    arg = int(input("What number do you want me to generate: "))
-    if 70 < arg < 100:
-        main(arg)
-        input("\nPress Enter to continue...")
-    else:
-        print("Error: Your number must be in the range of 71 - 99")
+    try:
+        arg = int(input("What number do you want me to generate: "))
+        if 70 < arg < 100:
+            main(arg)
+            input("\nPress Enter to continue...")
+        else:
+            print("Error: Your number must be in the range of 71 - 99")
+            input("\nPress Enter to continue...")
+    except ValueError:
+        print("Error: Invalid input. Please enter an integer.")
         input("\nPress Enter to continue...")
     clear_console()

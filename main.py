f18e455d19a6aaac365feec3ebc8c0b6c4532fa7
kirato <capacity1412@gmail.com>
2023-06-08 23:31:20 +0700

    optimized

--- main.py
+++ main.py
@@ -4,28 +4,24 @@ import os
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def main(*args)->void:
-    results = []
+def main(arg):
+    result = [random.randint(30, 40), random.randint(30, 40)]
+    result.append(arg - sum(result))
     
-    for arg in args:
+    while not (12 <= result[2] <= 20):
         result = [random.randint(30, 40), random.randint(30, 40)]
-        result.append(arg - (result[0] + result[1]))
-        
-        while result[2] < 12 or result[2] > 20:
-            result = [random.randint(30, 40), random.randint(30, 40)]
-            result.append(arg - (result[0] + result[1]))
-        
-        results.append(result)
-        print(result)
+        result.append(arg - sum(result))
+    
+    print(result)
 
 clear_console()
 
 while True:
     arg = int(input("What number do you want me to generate: "))
-    if(70<arg<100):
+    if 70 < arg < 100:
         main(arg)
         input("\nPress Enter to continue...")
     else:
-        print("Error : Your number must in range 71 - 99")
+        print("Error: Your number must be in the range of 71 - 99")
         input("\nPress Enter to continue...")
     clear_console()
